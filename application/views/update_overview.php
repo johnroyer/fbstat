db760f581d8e889f29494d0cb386435cb7f6a84e
Zero <john.royer@gmail.com>
2012-03-03 11:47:15 +0800

    資料庫異動概況頁面

---
+++ application/views/update_overview.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+   <title>Database Update</title>
+   
+</head>
+<body>
+   <h2>Database update complete</h2>
+   <blockquote>
+      <p>Database has updated.</p>
+      <p>Update overview:
+         <ul>
+            <li>New post: 3</li>
+            <li>New comment: 10</li>
+         </ul>
+      </p>
+   </blockquote>
+</body>
+</html>
