dd7af2adfa8ce6cc31b7dd3673047e792549c938
Zero <john.royer@gmail.com>
2012-03-12 13:06:24 +0800

    Articles Cleanning
    Adding likes data into array

--- application/libraries/Cleaner.php
+++ application/libraries/Cleaner.php
@@ -9,13 +9,28 @@ class Cleaner {
       }
 
       $articles = $in['data'];
+      $out = $this->getArticles($articles);
 
-      return $articles;
+      return $out;
+   }
+
+   private function getArticles($in){
+      foreach( $in as $val ){
+         $art = array(
+            'id' => $val['id'],
+            'user_id' => $val['from']['id'],
+            'message' => $val['message'], 
+            'link' => array_key_exists('link',$val) ? $val['link'] : '',
+            'like' => $this->getLikes( $val['likes'] )
+         );
+         $list[] = $art;
+      }
+      return $list;
    }
 
    private function getLikes($in){
       $list = $in['data'];
-      foreach( $in as $val ){
+      foreach( $list as $val ){
          $out[] = $val['id'];
       }
       return $out;
